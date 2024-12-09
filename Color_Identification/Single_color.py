c83c816e5dbb875a02eb40caf557b80e55d91528
Nasser Abdulwahab <122320807+ChibiDemon@users.noreply.github.com>
2024-12-09 18:50:21 -0500

    Update Single_color.py

--- Color_Identification/Single_color.py
+++ Color_Identification/Single_color.py
@@ -1,21 +1,30 @@
 import cv2
 import numpy as np
 
+# Defining lower and upper bounds of the color blue
 blue_lower = np.array([98, 50, 50])
 blue_upper = np.array([139, 255, 255])
 
+# Starting video Capture
 cap = cv2.VideoCapture(0)
 
 while True:
+    # Reading in the current frame
     ret, frame = cap.read()
     if not ret:
         break
 
+    #changing formate to hsv
     into_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    # creating the binary mask that only includes the volor blue
     blue_mask = cv2.inRange(into_hsv, blue_lower, blue_upper)
+
+    # Defining the contours of the bitmask
     contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    
+    # Defining text parameters
     font = cv2.FONT_HERSHEY_SIMPLEX
     color_text = (255, 255, 255)
 
@@ -34,8 +43,10 @@ while True:
         text_position = (cX - 50, cY + 10)
         cv2.putText(frame, "Blue", text_position, font, 1, color_text, 2)
 
+    # Displaying the manipulated fram
     cv2.imshow('Detected Blue Color', frame)
 
+    # Checks to see if the escape key was pressed
     if cv2.waitKey(1) == 27:
         break
 
