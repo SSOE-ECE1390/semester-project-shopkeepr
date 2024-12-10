c5e6d58409c9bf881118c0ba32ad55b01580ece6
Nasser Abdulwahab <122320807+ChibiDemon@users.noreply.github.com>
2024-12-09 19:04:36 -0500

    Update muli_color.py

--- Color_Identification/muli_color.py
+++ Color_Identification/muli_color.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+# Defining the upper and lower limits of the colors we want to look for
 color_ranges = {
     "Blue": ([98, 50, 50], [139, 255, 255]),
     "Red": ([0, 100, 100], [10, 255, 255]),
@@ -8,19 +9,27 @@ color_ranges = {
     "Brown": ([10, 80, 30], [25, 255, 180]),
 }
 
+# Starting video capture
 cap = cv2.VideoCapture(0)
 
 while True:
+
+    # Capturing the current frame
     ret, frame = cap.read()
     if not ret:
         break
 
+    # Turning it into HSV format
     hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    # Looping through each color's upper and lower limits
     for name, (lower_limit, upper_limit) in color_ranges.items():
+
+        # Creating the colors bitmask and contours
         color_mask = cv2.inRange(hsv_image, np.array(lower_limit), np.array(upper_limit))
         contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        # Defining font parameters
         font = cv2.FONT_HERSHEY_SIMPLEX
         color_text = (255, 255, 255)
 
@@ -40,8 +49,10 @@ while True:
             text_position = (cX - 50, cY + 10)
             cv2.putText(frame, name, text_position, font, 1, color_text, 2)
 
+    # Displaying the manipulated fram
     cv2.imshow("Detected",frame)
 
+    # Checking to see if esc key was pressed
     if cv2.waitKey(1) == 27:
         break
 
